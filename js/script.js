029fa117b44bfe5cef55db4be2882d1d45d1eac2
siska199 <siska.apriana.1703226@students.um.ac.id>
2022-10-25 16:10:13 +0700

    create kendoTemplate v1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
-$(function(){
-    
-})
+$(function () {
+  const data = [
+    { product: "Dresses", isShipped: true, colors: ["red", "pink", "blue"] },
+    {
+      product: "T-shirts",
+      isShipped: false,
+      colors: ["green", "orange", "blue", "purple"],
+    },
+    {
+      product: "Trousers",
+      isShipped: true,
+      colors: ["brown", "lime", "lightblue"],
+    },
+  ];
+
+  //Extract the template content from script tag:
+  const templateContent = $("#myTemplate").html();
+  //Compile a template
+  const template = kendo.template(templateContent);
+  const result = kendo.render(template, data);
+  //Where you while render or show it:
+  $("#renderTemplate").html(result);
+});
